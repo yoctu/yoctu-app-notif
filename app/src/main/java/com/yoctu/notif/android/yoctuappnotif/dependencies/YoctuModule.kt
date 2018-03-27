931a99784a5b53ac7d0b17d091f5488fc574ee6b
Gaël TSHILOMBO WANTICALE <gtshilombowanticale@adneom.com>
2018-03-27 13:19:35 +0200

    feat(): Add code for google sign in

--- app/src/main/java/com/yoctu/notif/android/yoctuappnotif/dependencies/YoctuModule.kt
+++ app/src/main/java/com/yoctu/notif/android/yoctuappnotif/dependencies/YoctuModule.kt
@@ -1,9 +1,11 @@
 package com.yoctu.notif.android.yoctuappnotif.dependencies
 
+import android.content.Context
 import com.github.salomonbrys.kodein.Kodein
 import com.github.salomonbrys.kodein.bind
 import com.github.salomonbrys.kodein.multiton
 import com.github.salomonbrys.kodein.singleton
+import com.yoctu.notif.android.yoctuappnotif.managers.ManageGoogleSignin
 import com.yoctu.notif.android.yoctuappnotif.repository.YoctuRepository
 import com.yoctu.notif.android.yoctuappnotif.ui.login.LoginContract
 import com.yoctu.notif.android.yoctuappnotif.ui.login.LoginFragment
@@ -24,6 +26,9 @@ object YoctuModule {
         // ** log in **
         bind<LoginContract.Presenter>() with singleton { LoginPresenter() }
         bind<LoginFragment>() with singleton { LoginFragment() }
+
+        //managers
+        bind<ManageGoogleSignin>() with multiton { context: Context -> ManageGoogleSignin(context) }
 
         //retrofit
         bind<YoctuService>() with  multiton { retrofit: Retrofit -> retrofit.create(YoctuService::class.java) }
