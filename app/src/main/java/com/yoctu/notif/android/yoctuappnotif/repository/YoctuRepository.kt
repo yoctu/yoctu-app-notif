48788bc6fb9e8e4eaa2bf48a043c16f408d2e1d8
Gaël TSHILOMBO WANTICALE <gtshilombowanticale@adneom.com>
2018-03-27 14:05:56 +0200

    feat(): Add and implement new functions

--- app/src/main/java/com/yoctu/notif/android/yoctuappnotif/repository/YoctuRepository.kt
+++ app/src/main/java/com/yoctu/notif/android/yoctuappnotif/repository/YoctuRepository.kt
@@ -1,10 +1,12 @@
 package com.yoctu.notif.android.yoctuappnotif.repository
 
+import android.content.Context
 import com.github.salomonbrys.kodein.instance
 import com.github.salomonbrys.kodein.with
 import com.yoctu.notif.android.yoctuappnotif.YoctuApplication
 import com.yoctu.notif.android.yoctulibrary.models.User
 import com.yoctu.notif.android.yoctulibrary.repository.Repository
+import com.yoctu.notif.android.yoctulibrary.repository.manager.ManagerSharedPreferences
 import com.yoctu.notif.android.yoctulibrary.repository.retrofit.YoctuService
 import io.reactivex.Observable
 import io.reactivex.Observer
@@ -12,11 +14,18 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 
 /**
+ * Repository to save and get models
+ *
  * Created on 26.03.18.
  */
 
-class YoctuRepository : Repository {
+class YoctuRepository(context: Context) : Repository {
+    private var mContext: Context
+    init {
+        mContext = context
+    }
     private val request : YoctuService = YoctuApplication.kodein.with(YoctuApplication.retrofit).instance()
+    private val localManager : ManagerSharedPreferences = YoctuApplication.kodein.with(mContext).instance()
 
     override fun getChannels(observer: Observer<Any>) {
         val obs = request.getChannels()
@@ -30,8 +39,9 @@ class YoctuRepository : Repository {
     //TODO
     override fun saveDeviceId(email: String, token: String, observer: Observer<Any>) {}
 
-    //TODO
-    override fun saveUser(user: User) {}
+    override fun saveUser(user: User) {
+        localManager.saveUser(user)
+    }
 
     fun setObservable(observable: Observable<Any>, subscriber : Observer<Any>) {
         observable
@@ -39,5 +49,11 @@ class YoctuRepository : Repository {
                 .observeOn(AndroidSchedulers.mainThread()) //observer
                 .subscribe(subscriber)
 
+    }
+
+    override fun getUser() = localManager.getUser()
+
+    override fun deleteUser() {
+        localManager.deleteUser()
     }
 }
