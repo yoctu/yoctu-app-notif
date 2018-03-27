ec9bd39b37af74c2165d76dfc4bfa79aff405ac8
Gaël TSHILOMBO WANTICALE <gtshilombowanticale@adneom.com>
2018-03-27 13:15:01 +0200

    feat(): Add a code for Google sign in

--- yoctulibrary/src/main/java/com/yoctu/notif/android/yoctulibrary/repository/retrofit/YoctuService.kt
+++ yoctulibrary/src/main/java/com/yoctu/notif/android/yoctulibrary/repository/retrofit/YoctuService.kt
@@ -1,8 +1,13 @@
 package com.yoctu.notif.android.yoctulibrary.repository.retrofit
 
 import com.yoctu.notif.android.yoctulibrary.models.Channel
+import com.yoctu.notif.android.yoctulibrary.models.ParamBodyDeviceID
+import com.yoctu.notif.android.yoctulibrary.models.ResponseChannels
 import io.reactivex.Observable
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
 
 /**
  * Contains all urls which will be used by Retrofit
@@ -13,5 +18,9 @@ import retrofit2.http.GET
 interface YoctuService {
 
     @GET("/api/channels")
-    fun getChannels(): Observable<List<Channel>>
+    fun getChannels(): Observable<ResponseChannels>
+
+    @Headers("Content-Type: application/json")
+    @POST("/api/notifications")
+    fun sendDeviceId(@Body params : ParamBodyDeviceID)
 }
