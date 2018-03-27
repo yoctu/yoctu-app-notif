73990466a67ae44ef23bdaea379ce180a7a1e0d0
Gaël TSHILOMBO WANTICALE <gtshilombowanticale@adneom.com>
2018-03-27 14:47:30 +0200

    feat(): Add a callback to communicate with fragment

---
+++ app/src/main/java/com/yoctu/notif/android/yoctuappnotif/callback/CallbackBroadcast.kt
@@ -0,0 +1,13 @@
+package com.yoctu.notif.android.yoctuappnotif.callback
+
+/**
+ * This callback allows the communication with the fragment/activity
+ *
+ * Created on 27.03.18.
+ */
+
+interface CallbackBroadcast {
+
+    fun getMessage(message : String)
+    fun getToken(token : String)
+}
