b8429f7036fd875576a4f3b89c3cebad2604869a
Gaël TSHILOMBO WANTICALE <gtshilombowanticale@adneom.com>
2018-03-28 15:12:01 +0200

    refactor(): add logic to manage user state

--- app/src/main/java/com/yoctu/notif/android/yoctuappnotif/ui/login/LoginActivity.kt
+++ app/src/main/java/com/yoctu/notif/android/yoctuappnotif/ui/login/LoginActivity.kt
@@ -3,8 +3,10 @@ package com.yoctu.notif.android.yoctuappnotif.ui.login
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.github.salomonbrys.kodein.instance
+import com.github.salomonbrys.kodein.with
 import com.yoctu.notif.android.yoctuappnotif.R
 import com.yoctu.notif.android.yoctuappnotif.YoctuApplication
+import com.yoctu.notif.android.yoctuappnotif.ui.notification.NotificationActivity
 import com.yoctu.notif.android.yoctuappnotif.utils.YoctuUtils
 
 /**
@@ -13,11 +15,32 @@ import com.yoctu.notif.android.yoctuappnotif.utils.YoctuUtils
 
 class LoginActivity : AppCompatActivity() {
 
+    private var loginPresenter : LoginContract.Presenter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        loginPresenter = YoctuApplication.kodein.with(this).instance()
+
         setContentView(R.layout.activity_login)
 
+        displayGoodView()
+    }
+
+    private fun displayGoodView() {
+        loginPresenter?.let {
+            val currentUser = loginPresenter!!.getUser()
+            val currentToppics = loginPresenter!!.getToppics()
+
+            if ( (currentUser == null) || (currentUser != null && currentToppics == null) ) { //first time OR no currentToppics
+                displayLoginFragment()
+            } else if (currentUser != null && currentToppics != null) {
+                startActivity(NotificationActivity.newIntent(this))
+            }
+        }
+    }
+
+    private fun displayLoginFragment() {
         var loginFragment : LoginFragment? = YoctuUtils.getFragment(supportFragmentManager,R.id.login_container_fragment) as? LoginFragment
         if(loginFragment == null) {
             loginFragment = YoctuApplication.kodein.instance()
