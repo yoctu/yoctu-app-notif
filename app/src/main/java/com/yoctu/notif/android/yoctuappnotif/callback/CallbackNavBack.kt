113b5c0e0f16475c7446046bea92f1897896975f
Gaël TSHILOMBO WANTICALE <gtshilombowanticale@adneom.com>
2018-03-29 17:55:01 +0200

    feat(): Add a callback for back nav

---
+++ app/src/main/java/com/yoctu/notif/android/yoctuappnotif/callback/CallbackNavBack.kt
@@ -0,0 +1,10 @@
+package com.yoctu.notif.android.yoctuappnotif.callback
+
+/**
+ * To manage back navigation into toolbar
+ *
+ * Created on 29.03.18.
+ */
+interface CallbackNavBack {
+    fun goBack()
+}
