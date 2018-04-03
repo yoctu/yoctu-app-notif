cdf0c0b5fdfd902a79e7e89a5cb84dd1b2ae435b
Gaël TSHILOMBO WANTICALE <gtshilombowanticale@adneom.com>
2018-04-03 14:28:16 +0200

    feat(): Add id to model

--- yoctulibrary/src/main/java/com/yoctu/notif/android/yoctulibrary/models/Channel.kt
+++ yoctulibrary/src/main/java/com/yoctu/notif/android/yoctulibrary/models/Channel.kt
@@ -1,16 +1,26 @@
 package com.yoctu.notif.android.yoctulibrary.models
 
+import com.google.gson.annotations.Expose
+
 /**
  * Created on 26.03.18.
  */
 
 class Channel() : ViewType{
 
+    @Expose
+    var id: Int = 0
+    @Expose
     var name : String = ""
     var checked : Boolean = false
 
     constructor(name : String) : this() {
         this.name = name
+    }
+
+    constructor(id: Int, name : String) : this() {
+        this.name = name
+        this.id = id
     }
 
     override fun getViewType() = ConstantsViewType.VIEW_TYPE_CHANNEL
