24aa4a2f09a2745d724cf6a28da36ce21d4a730a
Gaël TSHILOMBO WANTICALE <gtshilombowanticale@adneom.com>
2018-04-06 10:40:50 +0200

    feat(): Callback to reload a list of notification when app is background or kill

---
+++ app/src/main/java/com/yoctu/notif/android/yoctuappnotif/callback/CallbackReloadNotification.kt
@@ -0,0 +1,10 @@
+package com.yoctu.notif.android.yoctuappnotif.callback
+
+/**
+ * This callback allows to reload a list of notification
+ *
+ * Created  on 06.04.18.
+ */
+interface CallbackReloadNotification {
+    fun reload()
+}
