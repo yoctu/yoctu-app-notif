7736ca8524f426145dfb2ed4f64f67bc15f9aa80
malcom <gaeltshilombo@gmail.com>
2018-05-16 11:10:34 +0200

    feat(): init the view

--- app/src/main/java/com/yoctu/notif/android/yoctuappnotif/ui/add/AddTopicURLActivity.kt
+++ app/src/main/java/com/yoctu/notif/android/yoctuappnotif/ui/add/AddTopicURLActivity.kt
@@ -1,6 +1,11 @@
 package com.yoctu.notif.android.yoctuappnotif.ui.add
 
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.yoctu.notif.android.yoctuappnotif.R
+import com.yoctu.notif.android.yoctuappnotif.utils.YoctuUtils
 
 /**
  * Created by gael on 16.05.18.
@@ -8,4 +13,23 @@ import android.support.v7.app.AppCompatActivity
 
 class AddTopicURLActivity: AppCompatActivity() {
 
+    companion object {
+
+        fun newInten(context: Context) = context.startActivity(Intent(context, AddTopicURLActivity::class.java))
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_add_topic_url)
+
+        var addTopicURLFragment : AddTopicURLFragment? = YoctuUtils.getFragment(supportFragmentManager, R.id.activity_add_topic_container_fragment) as? AddTopicURLFragment
+        if (addTopicURLFragment == null) {
+            addTopicURLFragment = AddTopicURLFragment.newInstance()
+            YoctuUtils.addFragment(
+                    supportFragmentManager,
+                    addTopicURLFragment,
+                    R.id.activity_add_topic_container_fragment)
+        }
+    }
 }
--- app/src/main/java/com/yoctu/notif/android/yoctuappnotif/ui/add/AddTopicURLFragment.kt
+++ app/src/main/java/com/yoctu/notif/android/yoctuappnotif/ui/add/AddTopicURLFragment.kt
@@ -1,6 +1,22 @@
 package com.yoctu.notif.android.yoctuappnotif.ui.add
 
+import android.content.Context
+import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.Toolbar
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.github.salomonbrys.kodein.instance
+import com.github.salomonbrys.kodein.with
+import com.yoctu.notif.android.yoctuappnotif.R
+import com.yoctu.notif.android.yoctuappnotif.YoctuApplication
+import com.yoctu.notif.android.yoctuappnotif.ui.adapters.YoctuAdapter
+import com.yoctu.notif.android.yoctuappnotif.utils.YoctuUtils
+import kotlinx.android.synthetic.main.default_toolbar.*
+import kotlinx.android.synthetic.main.fragment_add_topic_url_fragment.*
 
 /**
  * Created by gael on 16.05.18.
@@ -8,4 +24,54 @@ import android.support.v4.app.Fragment
 
 class AddTopicURLFragment: Fragment() {
 
+    companion object {
+
+        fun newInstance() = AddTopicURLFragment()
+    }
+    private var addPresenter : AddTopicURLContract.Presenter? = null
+    private lateinit var toolbar : Toolbar
+    private lateinit var recyclerView : RecyclerView
+    private lateinit var adapter : YoctuAdapter
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    private fun getLayoutResId() = R.layout.fragment_add_topic_url_fragment
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(getLayoutResId(),container,false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        addPresenter = YoctuApplication.kodein.with(activity).instance()
+        manageToolbar()
+    }
+
+    private fun manageToolbar() {
+        add_topic_url_fragment_toolbar?.let { t ->
+            toolbar = t
+            (activity as AppCompatActivity).setSupportActionBar(toolbar)
+
+            YoctuUtils.changeToolbarColor(toolbar,activity!!.resources.getColor(R.color.colorPrimaryNoActionBar))
+            toolbar_standard_title?.let {
+                toolbar_standard_title.text = (activity as AppCompatActivity).getString(R.string.add_opic_url_view_title)
+            }
+
+            toolbar_standard_back_nav?.let {
+                toolbar_standard_back_nav.visibility = View.GONE
+                //callbackNav?.let { toolbar_standard_back_nav.setOnClickListener { _ -> callbackNav!!.goBack() } }
+            }
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+    }
 }
