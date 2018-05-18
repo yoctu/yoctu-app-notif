c3255af1227513b1685c55fa211e06e60f46be7b
malcom <gaeltshilombo@gmail.com>
2018-05-18 13:47:30 +0200

    refactor(): manage the view and events

--- app/src/main/java/com/yoctu/notif/android/yoctuappnotif/ui/add/AddTopicURLFragment.kt
+++ app/src/main/java/com/yoctu/notif/android/yoctuappnotif/ui/add/AddTopicURLFragment.kt
@@ -1,24 +1,30 @@
 package com.yoctu.notif.android.yoctuappnotif.ui.add
 
+import android.app.Activity
 import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.Toolbar
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.Toast
 import com.github.salomonbrys.kodein.instance
 import com.github.salomonbrys.kodein.with
 import com.yoctu.notif.android.yoctuappnotif.R
 import com.yoctu.notif.android.yoctuappnotif.YoctuApplication
 import com.yoctu.notif.android.yoctuappnotif.ui.adapters.YoctuAdapter
+import com.yoctu.notif.android.yoctuappnotif.utils.KeyboardUtils
 import com.yoctu.notif.android.yoctuappnotif.utils.YoctuUtils
 import com.yoctu.notif.android.yoctulibrary.repository.manager.ManagerSharedPreferences
 import kotlinx.android.synthetic.main.default_toolbar.*
 import kotlinx.android.synthetic.main.fragment_add_topic_url_fragment.*
+import kotlinx.android.synthetic.main.topic_fragment.*
 
 /**
  * Created by gael on 16.05.18.
@@ -63,11 +69,11 @@ class AddTopicURLFragment:
         }
         manageToolbar()
         manageClickEvents()
-        //manageView()
+        manageView()
     }
 
-    private fun checkTopicURL(context: Context) {
-        managerSharedPreferences = YoctuApplication.kodein.with(context).instance()
+    private fun checkTopicURL(activity: Activity) {
+        managerSharedPreferences = YoctuApplication.kodein.with(activity.applicationContext).instance()
         managerSharedPreferences?.let { shared ->
             currentTopicURL = shared.getTopicURL()
 
@@ -77,6 +83,7 @@ class AddTopicURLFragment:
                 }
                 add_topic_url_button?.let { button -> button.text = activity?.let { it.resources.getString(R.string.add_topic_url_current_url_save_new_url_btn) } }
             } else {
+                add_topic_url_fragment_current_url?.let { v -> v.text = currentTopicURL }
                 add_topic_url_linear_layout_horizontal?.let { container ->
                     container.visibility = View.VISIBLE
                 }
@@ -117,18 +124,22 @@ class AddTopicURLFragment:
     }
 
     private fun manageView() {
-        //simulate
-        Thread.sleep(3000)
-        add_topic_url_fragment_edit_text?.let { v ->
-            addPresenter?.let { presenter ->
-                presenter.saveTopicURL("http://notification.test.flash-global.net/api/channels")
-            }
+        add_topic_url_fragment_principal_container?.let { v ->
+            v.setOnTouchListener(object : View.OnTouchListener{
+                override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+                    if (v !is EditText)
+                        add_topic_url_fragment_edit_text?.let { edit -> KeyboardUtils.hidesKeyboard(edit) }
+
+                    return false
+                }
+            })
         }
     }
 
     override fun showErrorMessage(message: String) {
         activity?.let { act ->
-            Toast.makeText(act,message,Toast.LENGTH_SHORT).show()
+            //Toast.makeText(act,message,Toast.LENGTH_SHORT).show()
+            YoctuUtils.displaySnackBar(add_topic_url_button, act.getString(R.string.login_fragment_there_are_not_chosen))
         }
     }
 
--- app/src/main/java/com/yoctu/notif/android/yoctuappnotif/ui/add/AddTopicURLPresenter.kt
+++ app/src/main/java/com/yoctu/notif/android/yoctuappnotif/ui/add/AddTopicURLPresenter.kt
@@ -2,8 +2,12 @@ package com.yoctu.notif.android.yoctuappnotif.ui.add
 
 import android.content.Context
 import android.util.Log
+import com.github.salomonbrys.kodein.instance
+import com.github.salomonbrys.kodein.with
 import com.yoctu.notif.android.yoctuappnotif.R
+import com.yoctu.notif.android.yoctuappnotif.YoctuApplication
 import com.yoctu.notif.android.yoctuappnotif.utils.YoctuUtils
+import com.yoctu.notif.android.yoctulibrary.repository.manager.ManagerSharedPreferences
 
 /**
  * Created by gael on 16.05.18.
@@ -15,6 +19,7 @@ class AddTopicURLPresenter(context: Context): AddTopicURLContract.Presenter {
     init {
         mContext = context
     }
+    private var managerSharedPreferences: ManagerSharedPreferences = YoctuApplication.kodein.with(mContext.applicationContext).instance()
 
     override fun takeView(view: AddTopicURLContract.View) {
         this.mView = view
@@ -23,6 +28,7 @@ class AddTopicURLPresenter(context: Context): AddTopicURLContract.Presenter {
     override fun saveTopicURL(url: String) {
         if (!url.isEmpty() && YoctuUtils.isValidScheme(url)) {
             Log.d(YoctuUtils.TAG_DEBUG," TRUE ")
+            managerSharedPreferences.saveTopicURL(url)
         } else {
             Log.d(YoctuUtils.TAG_DEBUG," FALSE ")
             mView?.let { v -> v.showErrorMessage(mContext.resources.getString(R.string.add_topic_url_error_message_not_valid)) }
